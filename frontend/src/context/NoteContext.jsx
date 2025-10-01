bdca4825e3c5e5ad721391060ba7220c254c39d4
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-10-01 23:52:00 +0530

    done

--- frontend/src/context/NoteContext.jsx
+++ frontend/src/context/NoteContext.jsx
@@ -11,7 +11,8 @@ const [loading, setLoading] = useState(true);
 const getNotes = async() => {
     setLoading(true);
     try {
-        const response = await BACKEND_URL.get("/get-notes");
+        const response = await BACKEND_URL.get("/api/v1/noteapp/get-notes");
+    //   await BACKEND_URL.get("/get-notes");
         setNotes(response.data);
     } catch (error) {
         console.error("Error fetching notes:", error);
@@ -26,19 +27,23 @@ useEffect(()=>{
 
 // create a note
 const createNote = async(note) => {
-    const res=await BACKEND_URL.post("/create-note",note)
+    const res=await BACKEND_URL.post("/api/v1/noteapp/create-note", note)
+    // await BACKEND_URL.post("/create-note",note)
     setNotes([res.data,...notes])
 }
 
 // update a note
 const updateNote = async(id, updateNote) => {
-    const res=await BACKEND_URL.put(`/update-note/${id}`,updateNote)
+    const res=await BACKEND_URL.put(`/api/v1/noteapp/update-note/${id}`, updateNote)
+    // await BACKEND_URL.put(`/update-note/${id}`,updateNote)
     setNotes(notes.map((note)=>(note._id===id ? res.data : note)))
 }
 
 // delete a note
 const deleteNote = async(id) => {
-    await BACKEND_URL.delete(`/delete-note/${id}`)
+    // await BACKEND_URL.delete(`/delete-note/${id}`)
+    await BACKEND_URL.delete(`/api/v1/noteapp/delete-note/${id}`)
+
     setNotes(notes.filter((note)=>(note._id!==id)))
 }
 
